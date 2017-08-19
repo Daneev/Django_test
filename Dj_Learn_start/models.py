72b53712551ff8c5a3898b07bdcd1b5ad4a8a44f
Daneev <30390132+Daneev@users.noreply.github.com>
2017-08-20 03:50:31 +0400

    реализован поиск

--- Dj_Learn_start/models.py
+++ Dj_Learn_start/models.py
@@ -16,4 +16,3 @@ class Post(models.Model):
         """Возвращает строковое представление модели."""
         return u'{} {}'.format(self.title, self.date_create)
 
-    
--- Dj_Learn_start/views.py
+++ Dj_Learn_start/views.py
@@ -4,7 +4,12 @@ from Dj_Learn_start.models import Post
 
 def post_list(request):
     post_list = Post.objects.all()
-    return render_to_response("blog/post_list.html", {"post_list": post_list})
+
+    query = request.GET.get("query")
+    if query:
+        post_list = post_list.filter(text__icontains=query)
+
+    return render_to_response("blog/post_list.html", {"post_list": post_list, "query": query})
 
 def post_detail(request, pk):
     post = Post.objects.get(id=pk)
