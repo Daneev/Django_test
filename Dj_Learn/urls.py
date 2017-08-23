40cf84d1441a961de89361044ff0f80b702d82ae
Daneev <30390132+Daneev@users.noreply.github.com>
2017-08-24 02:23:01 +0400

    разделение url

--- Dj_Learn/urls.py
+++ Dj_Learn/urls.py
@@ -15,16 +15,22 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from Apteki.views import lekarstv_list, lekarstv_detail
+from Apteki.views import list, detail
 from Dj_Learn_start.views import post_detail, test, post_list
 from django.conf.urls.static import static
 from django.conf import settings
+
+
+lekpatterns = [
+    url(r'^list/', list, name='list'),
+    url(r'^detail/(?P<lekarstv_id>\d+)/$', detail, name='detail'),
+]
 
 urlpatterns = [
     url(r'^$', post_list, name='post_list'),
     url(r'^test/', test, name='test'),
     url(r'^post/(?P<pk>\d+)/$', post_detail, name='post_detail'),
     url(r'^admin/', admin.site.urls),
-    url(r'^lekarstv_list/', lekarstv_list, name='lekarstv_list'),
-    url(r'^lekarstv/(?P<lekarstv_id>\d+)/$', lekarstv_detail, name='lekarstv_detail'),
+    url(r'^lekarstv/', include(lekpatterns)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
